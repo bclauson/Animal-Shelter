0eb2143b23f077e5a8ac2918a4d6209f55567d0a
bclauson <clauson.branden@gmail.com>
2022-04-23 14:06:34 -0700

    add set up files and animal model controller

---
+++ LocalAnimalShelter/Migrations/20220423210408_Initial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalAnimalShelter.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Species = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 2, "Male", "Joey", "Cat" },
+                    { 2, 5, "Male", "Max", "Dog" },
+                    { 3, 3, "Female", "Aquarius", "Cat" },
+                    { 4, 4, "Female", "Sadie", "Dog" },
+                    { 5, 3, "Male", "Rex", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
---
+++ LocalAnimalShelter/Models/LocalAnimalShelterContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LocalAnimalShelter.Models
+{
+    public class LocalAnimalShelterContext : DbContext
+    {
+        public LocalAnimalShelterContext(DbContextOptions<LocalAnimalShelterContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Animal>()
+              .HasData(
+                  new Animal { AnimalId = 1, Name = "Joey", Species = "Cat", Age = 2, Gender = "Male" },
+                  new Animal { AnimalId = 2, Name = "Max", Species = "Dog", Age = 5, Gender = "Male" },
+                  new Animal { AnimalId = 3, Name = "Aquarius", Species = "Cat", Age = 3, Gender = "Female" },
+                  new Animal { AnimalId = 4, Name = "Sadie", Species = "Dog", Age = 4, Gender = "Female" },
+                  new Animal { AnimalId = 5, Name = "Rex", Species = "Dog", Age = 3, Gender = "Male" }
+              );
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
