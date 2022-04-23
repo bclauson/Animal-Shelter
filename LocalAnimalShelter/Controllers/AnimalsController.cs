e2ec2f00c69565595b3989456506586b7e4b6cfe
bclauson <clauson.branden@gmail.com>
2022-04-23 14:19:40 -0700

    fixed seeding, added parameters to get requests

--- LocalAnimalShelter/Controllers/AnimalsController.cs
+++ LocalAnimalShelter/Controllers/AnimalsController.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,9 +23,26 @@ namespace LocalAnimalShelter.Controllers
 
     // GET: api/Animals
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    public async Task<ActionResult<IEnumerable<Animal>>> Get(string species, string gender, string name)
     {
-      return await _db.Animals.ToListAsync();
+      var query = _db.Animals.AsQueryable();
+
+      if (species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }    
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }      
+
+      return await query.ToListAsync();
     }
 
     // GET: api/Animals/5
--- LocalAnimalShelter/Models/LocalAnimalShelterContext.cs
+++ LocalAnimalShelter/Models/LocalAnimalShelterContext.cs
@@ -17,8 +17,8 @@ namespace LocalAnimalShelter.Models
                   new Animal { AnimalId = 2, Name = "Max", Species = "Dog", Age = 5, Gender = "Male" },
                   new Animal { AnimalId = 3, Name = "Aquarius", Species = "Cat", Age = 3, Gender = "Female" },
                   new Animal { AnimalId = 4, Name = "Sadie", Species = "Dog", Age = 4, Gender = "Female" },
-                  new Animal { AnimalId = 5, Name = "Rex", Species = "Dog", Age = 3, Gender = "Male" }
-                  new Animal { AnimalId = 6, Name = "Sally", Species = "Dog", Age = 1, Gender = "Female" }
+                  new Animal { AnimalId = 5, Name = "Rex", Species = "Dog", Age = 3, Gender = "Male" },
+                  new Animal { AnimalId = 6, Name = "Sally", Species = "Dog", Age = 1, Gender = "Female" },
                   new Animal { AnimalId = 7, Name = "Lex", Species = "Cat", Age = 2, Gender = "Female" }
               );
         }
