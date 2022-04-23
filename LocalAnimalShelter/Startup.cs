1578ef8590c74afb7e99bbb4f3c80ff49dc3215d
bclauson <clauson.branden@gmail.com>
2022-04-23 14:55:58 -0700

    add swagger documentation

--- LocalAnimalShelter/Startup.cs
+++ LocalAnimalShelter/Startup.cs
@@ -25,6 +25,22 @@ namespace LocalAnimalShelter
             services.AddDbContext<LocalAnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "Local Animal Shelter API";
+                    document.Info.Description = "Api to manage information for animals (specifically cats and dogs) at a shelter";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Branden Clauson",
+                        Email = "clauson.branden@gmail.com",
+                        Url = "https://github.com/bclauson"
+                    };
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,9 +49,16 @@ namespace LocalAnimalShelter
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //swagger usage line
+                app.UseOpenApi();
+                app.UseSwaggerUi3();
             }
 
             // app.UseHttpsRedirection();
+
+
+
+
 
             app.UseRouting();
 
